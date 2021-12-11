7683dd49ba437831368c104db5b9099cd49c3191
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-12-12 02:46:25 +0600

    create base page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import ParkingSpace, ReservingUser, Reserve, Profile
 
-# Register your models here.
+
+@admin.register(ParkingSpace)
+class ParkingSpaceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(ReservingUser)
+class ReservingUserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+
+@admin.register(Reserve)
+class ReserveAdmin(admin.ModelAdmin):
+    list_display = ['unique_id']
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
